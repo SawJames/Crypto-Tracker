7bf342293217d1156c4c7bcf97936389503e5039
Saw <tarmalar@gmail.com>
2021-09-15 20:28:40 +0630

    First Upload

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by saw Tarmalar on 15/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
