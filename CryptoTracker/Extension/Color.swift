47cff18c276b8088fe78b183a61c0ec257fa3c3c
Saw <tarmalar@gmail.com>
2021-09-15 20:37:22 +0630

    Add Image and Color theme

---
+++ CryptoTracker/Extension/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  CryptoTracker
+//
+//  Created by saw Tarmalar on 15/09/2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme{
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
